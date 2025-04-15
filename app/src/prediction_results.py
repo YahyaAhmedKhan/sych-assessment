aacaba3e8c6010b970ce3d5407a1476ffa994a49
Yahya Ahmed Khan <yahyaahmed.52@gmail.com>
2025-04-15 22:59:30 +0500

    made a singleton for results_dict

--- app/src/prediction_results.py
+++ app/src/prediction_results.py
@@ -1,13 +1,20 @@
 from models import TaskResult    
+class ResultsStore:
+    """
+    Used a singleton for this as well as there has to be dictionary.
+    Without this, if I did results_dict = {}, then it would make a new empty dictionary
+    every time this file was imported.
+    """
+    _instance = None
+    _results_dict: dict[str, TaskResult]
 
-results_dict: dict[str, TaskResult] = {
-    
-    # "1" : {
-    #     "status": "QUEUED",
-        
-    #     "task_result": {
-    #         "prediction_id": "abc123",
-    #         "output": {"input": "Sample input data for the model", "result": "5678"} 
-    #         }
-    # }
-}
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(ResultsStore, cls).__new__(cls)
+            cls._instance._results_dict = {}
+        return cls._instance
+
+    def get_results_dict(self) -> dict[str, TaskResult]:
+        return self._results_dict
+
+results_dict = ResultsStore().get_results_dict()
